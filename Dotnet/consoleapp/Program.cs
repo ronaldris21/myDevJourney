39130c96b892d723b286ac2b2af6c9e35e7a3d08
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2022-07-25 18:16:06 +0200

    Fixing Readme and trying markup language

--- Dotnet/consoleapp/Program.cs
+++ Dotnet/consoleapp/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Input your name: ");
 
-String name = Console.ReadLine();
+String? name = Console.ReadLine();
 System.Console.WriteLine($"Welcome back {name}!");
