8d91215d10506859ba13a65d0444c506334897a5
RonaldRis21 <ronald.tejada@oportunidades.org.sv>
2024-11-20 13:39:37 -0600

    Re uploading typescript async  exercises course notes

---
+++ Nerdery-Nodejs/typescript/typescript-async/xx-final/webpack.config.js
@@ -0,0 +1,71 @@
+/* eslint-disable */
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const index = 'index';
+
+module.exports = {
+  entry: `./src/${index}.ts`,
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `./${index}.html`,
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  performance: {
+    maxEntrypointSize: 640000,
+    maxAssetSize: 640000,
+  },
+  devServer: {
+    contentBase: 'dist',
+    port: 8080,
+    // Send API requests on localhost to API server get around CORS.
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
+};
